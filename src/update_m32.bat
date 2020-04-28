dca77251c31a4e12d92f1f4f565d72368aeb6892
Joe Wittmer <joseph.e.wittmer@gmail.com>
2020-04-28 10:13:54 -0500

    passes in baud rate as command line option

---
+++ src/update_m32.bat
@@ -0,0 +1,2 @@
+update_m32.exe COM5 115200 "C:\Users\%USERNAME%\Downloads\morse_3_v2.4.ino.wifi_lora_32.bin"
+pause
--- src/update_m32.py
+++ src/update_m32.py
@@ -4,34 +4,36 @@ from contextlib import redirect_stdout
 import io
 import os
 
-def update_morserino(port, path):
+def update_morserino(port, rate, path):
 	f = io.StringIO()
-	command = ['--port', port, '--baud', '921600', '--before', 'default_reset', '--after', 'hard_reset', 'write_flash', '0x10000', path]
+	command = ['--port', port, '--baud', rate, '--before', 'default_reset', '--after', 'hard_reset', 'write_flash', '0x10000', path]
 	with redirect_stdout(f):
 		esptool.main(command)
 	s = f.getvalue().rstrip()
 	suffix = "Hard resetting via RTS pin..."
 	return s.endswith(suffix)
 
-def main(port, path):
-    if update_morserino(port, path):
+def main(port, rate, path):
+    if update_morserino(port, rate, path):
         print("Firmware was updated successfully")
     else:
         print("Firmware update failed")
         
 if __name__ == "__main__":
-    if (len(sys.argv) > 2):
+    if (len(sys.argv) > 3):
         port = sys.argv[1]
-        path = sys.argv[2]
+        rate = sys.argv[2]
+        path = sys.argv[3]
         pathExists = os.path.exists(path)
         if (pathExists):
             filename = os.path.basename(path)
             print()
             print("Attempting to update Morserino-32")
             print("  Port: " + port)
+            print("  Rate: " + rate)
             print("  Firmware: " + filename)
             print("Please wait...")
-            main(port, path)
+            main(port, rate, path)
         else:
             print()
             print("Error opening path: " + path);
@@ -39,4 +41,4 @@ if __name__ == "__main__":
         print()
         print(sys.argv[0] + "error...")
         print("Please try again with the following command line options: ")
-        print("  " + sys.argv[0] + " {serial_port} " + "{file_path}")
+        print("  " + sys.argv[0] + " {serial_port}" + " {rate}" + " {file_path}")
