85e36a14d3fcf7cc0dc921513c6696d607775af7
Joe Wittmer <joseph.e.wittmer@gmail.com>
2025-09-09 12:42:39 -0500

    updated releases to include new bin folders

--- distribute.sh
+++ distribute.sh
@@ -1,1 +1,1 @@
-pyinstaller --clean -y --onefile --add-data "src/m32/*.bin:bin/" src/update_m32.py
+pyinstaller --clean -y --onefile --add-data "src/m32/*.bin:bin/" --add-data "src/m32pocket/*.bin:bin/" src/update_m32.py
