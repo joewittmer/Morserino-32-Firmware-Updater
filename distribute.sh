d21ebbd3457dd008dd7576eaca429c5741eba190
Joe Wittmer <joseph.e.wittmer@gmail.com>
2025-09-29 10:48:33 -0500

    fixed pyinstaller issues

--- distribute.sh
+++ distribute.sh
@@ -1,4 +1,5 @@
 pyinstaller --clean -y --onefile \
-		--add-data "src/m32/*.bin:m32/" \
-		--add-data "src/m32pocket/*.bin:m32pocket/" \
+	--collect-all esptool \
+	--add-data "src/m32/*.bin:m32/" \
+	--add-data "src/m32pocket/*.bin:m32pocket/" \
 	src/update_m32.py
